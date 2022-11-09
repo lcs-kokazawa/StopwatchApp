67b6b8008dcb869f8bd268ccd3922d908392c394
Kiho Okazawa <kokazawa@lcs.on.ca>
2022-11-09 09:17:32 -0500

    Initial Commit

---
+++ StopwatchApp/StopwatchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StopwatchAppApp.swift
+//  StopwatchApp
+//
+//  Created by Kiho Okazawa on 2022-11-09.
+//
+
+import SwiftUI
+
+@main
+struct StopwatchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
