a4c0fc24b85c90ae2dc3556e0b3d192e2bc7bdbd
Kiho Okazawa <kokazawa@lcs.on.ca>
2022-11-09 16:03:19 -0500

    Made the selected tab item default to the Stopwatch.

--- StopwatchApp/ContentView.swift
+++ StopwatchApp/ContentView.swift
@@ -36,7 +36,7 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        TabView {
+        TabView(selection: Binding.constant(3)) {
             
             Text("World Clock")
                 .tabItem {
